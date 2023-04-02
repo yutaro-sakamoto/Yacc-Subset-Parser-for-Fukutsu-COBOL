d73a4b3300769db73bb640162990ee698bcb8dac
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2023-04-02 20:41:41 +0900

    update: the parser can recognize `parser.y`

--- src/ast.rs
+++ src/ast.rs
@@ -1,17 +1,21 @@
+#[derive(Debug, Clone)]
 pub struct Yacc<'a> {
     pub rules: Vec<YRule<'a>>,
 }
 
+#[derive(Debug, Clone)]
 pub struct YRule<'a> {
     pub entry: &'a str,
     pub allow_empty: bool,
     pub tokens_list: Vec<YTokens<'a>>,
 }
 
+#[derive(Debug, Clone)]
 pub struct YTokens<'a> {
     pub tokens: Vec<YToken<'a>>,
 }
 
+#[derive(Debug, Clone)]
 pub enum YToken<'a> {
     Id(&'a str),
     Terminal(&'a str),
--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,11 @@ lalrpop_mod!(pub parser);
 mod ast;
 
 fn main() {
-    println!("Hello, world!");
+    let infile = "parser.y";
+    let source = std::fs::read_to_string(infile)
+        .expect(format!("[Error] input file `{}` not found", infile).as_str());
+    let cst = parser::YaccParserParser::new()
+        .parse(&source)
+        .expect("parse error");
+    println!("{:?}", cst);
 }
